cc9f7cbcd8fa0ec1c1ceeb76ba597c494f4f4d5e
Nimmy <nimmypatel98@gmail.com>
2017-02-10 11:04:51 +0530

    Blank comit for jsonParsing using API calling.

---
+++ app/src/main/java/com/example/nimmy/task7_apicalling/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.nimmy.task7_apicalling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadJson();
+    }
+
+    public void loadJson() {
+
+    }
+
+    {
+
+    }
+}
